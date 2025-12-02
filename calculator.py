a9665cf922401160de7b0c2a9ea8bd2d0b65782d
Anand-k07 <anandk262007@gmail.com>
2025-12-02 17:41:04 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,18 @@
+x = 1
+y = 2
+
+z = x + y
+
+print(z)
+# int is used to store whole numbers
+# float is used to store decimal numbers
+
+x = float(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(f"{x+y:,}") # , is used to format the number with comma
+print(f"{x*y:.2f}") # .2f is used to format the number with 2 decimal places
+
+
+
+
